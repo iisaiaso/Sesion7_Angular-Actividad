f85a4fb43ee0988a7c8a86be6098e3defd54effc
iisaiaso <isaiasibarraonofre@gmail.com>
2022-12-29 10:18:14 -0500

    Finalizado

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NavbarComponent,
+    HeaderComponent
+  ]
+})
+export class SharedModule { }
